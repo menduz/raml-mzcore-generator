bf84312779b04b4dc57dc738483ad4646ff987d6
Agustin Mendez (Menduz) <lord.yo.wo@gmail.com>
2016-03-01 11:14:23 -0300

    basic support

--- raml-javascript-generator.js
+++ raml-javascript-generator.js
@@ -5,16 +5,16 @@ var generator = require('raml-generator')
 module.exports = generator({
   templates: {
     '.gitignore': fs.readFileSync(path.join(__dirname, 'lib/templates/.gitignore.hbs'), 'utf8'),
-    'index.js': fs.readFileSync(path.join(__dirname, 'lib/templates/index.js.hbs'), 'utf8'),
+    'index.ts': fs.readFileSync(path.join(__dirname, 'lib/templates/index.ts.hbs'), 'utf8'),
     'README.md': fs.readFileSync(path.join(__dirname, 'lib/templates/README.md.hbs'), 'utf8'),
     'INSTALL.md': fs.readFileSync(path.join(__dirname, 'lib/templates/INSTALL.md.hbs'), 'utf8'),
     'package.json': fs.readFileSync(path.join(__dirname, 'lib/templates/package.json.hbs'), 'utf8')
   },
   partials: {
-    auth: fs.readFileSync(path.join(__dirname, 'lib/partials/auth.js.hbs'), 'utf8'),
-    utils: fs.readFileSync(path.join(__dirname, 'lib/partials/utils.js.hbs'), 'utf8'),
-    client: fs.readFileSync(path.join(__dirname, 'lib/partials/client.js.hbs'), 'utf8'),
-    resources: fs.readFileSync(path.join(__dirname, 'lib/partials/resources.js.hbs'), 'utf8')
+    auth: fs.readFileSync(path.join(__dirname, 'lib/partials/auth.ts.hbs'), 'utf8'),
+    utils: fs.readFileSync(path.join(__dirname, 'lib/partials/utils.ts.hbs'), 'utf8'),
+    client: fs.readFileSync(path.join(__dirname, 'lib/partials/client.ts.hbs'), 'utf8'),
+    resources: fs.readFileSync(path.join(__dirname, 'lib/partials/resources.ts.hbs'), 'utf8')
   },
   helpers: {
     stringify: require('javascript-stringify'),
